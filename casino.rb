ca3e88734ae97bdeb183b9c73eb28770a655f327
Crys <chsantiago4@gmail.com>
2018-11-09 15:48:57 -0700

    add cointoss

--- casino.rb
+++ casino.rb
@@ -1,13 +1,13 @@
 require_relative './Player'
 
-class Casino
+class Casino < Player
     
     def initialize
-        Player.new
-        menu
+        super()
+        casino_menu
     end
     
-    def menu
+    def casino_menu
         puts "$$$ Menu $$$"
         puts "1. Crys"
         puts "2. Emilio"
